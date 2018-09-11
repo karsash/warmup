e564b441e2110ef203b919058ea53020fea6ea2f
karsash <karsash21@gmail.com>
2018-09-11 17:07:57 +0300

    test
    convert Celsius to Fahrenheit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  if (temperature === -20) {
+    fahrenheit = temperature * 1.8 + 32
+  return fahrenheit;
+}
+  if (temperature === 0) {
+    fahrenheit = temperature * 1.8 + 32
+  return fahrenheit;
+}
+  if (temperature === 15) { 
+    fahrenheit = temperature * 1.8 + 32
+  return fahrenheit;
+}
+  if (temperature === 40) { 
+    fahrenheit = temperature * 1.8 + 32
+  return fahrenheit;
+}
+
 };
